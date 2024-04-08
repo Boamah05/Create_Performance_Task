a8df717d75f390a745f1db85d5db7c481ac8a174
Boamah05 <143358964+Boamah05@users.noreply.github.com>
2024-04-08 13:03:22 +0000

    git commit

--- college_lists.py
+++ college_lists.py
@@ -1,101 +1,100 @@
-locations = ['Austin','Houston','Dallas', 'Fort Worth', 'Destin','San Antonio','Waco','Kileen', 'Corpus Christi']
-
+locations = [
+    'Austin', 'Houston', 'Dallas', 'Fort Worth', 'Destin', 'San Antonio',
+    'Waco', 'Kileen', 'Corpus Christi'
+]
 
 austin = {
-        "name" : "University of Texas at Austin",
-        "location": "Austin, TX",
-        "SAT_range": 1355,
-        "average_GPA": 3.73,
-    },{
-        "name": "Austin Community College",
-        "location": "Austin, TX",
-        "SAT_range": 1080,
-        "average_GPA": 2.0,
-    }
-          
+    "name": "University of Texas at Austin",
+    "location": "Austin, TX",
+    "SAT_range": 1355,
+    "average_GPA": 3.73,
+}, {
+    "name": "Austin Community College",
+    "location": "Austin, TX",
+    "SAT_range": 1080,
+    "average_GPA": 2.0,
+}
 
 houston = {
-        "name" : "University of Houston", 
-        "location": "Houston, TX",
-        "SAT_range": 1240,
-        "average_GPA": 3.5,
-    },{
-        "name" : "Houston Community College",
-        "location" : "Houston, TX",
-        "SAT_range": 1190,
-        "average_GPA": 3.5,
-    },{
-        "name" : "University of Houston–Downtown",
-        "location": "Houston, TX",
-        "SAT_range": 1240,
-        "average_GPA": 2.1,
-    },{
-        "name" : "Baylor College of Medicine",
-        "location": "Houston, TX",
-        "SAT_range": 1430,
-        "average_GPA": 3.91,
-    },{
-        "name" : "University of Houston–The Hill",
-        "location": "Houston, TX",
-        "SAT_range": 1240,
-        "average_GPA": 3.5,
-    },{
-        "name" : "University of Houston–Clear Lake",
-        "location": "Houston, TX",
-        "SAT_range": 1240,
-        "average_GPA": 3.5,
-    },{
-        "name" : "University of Texas Health Science Center at Houston",
-        "location": "Houston, TX",
-        "SAT_range": 1190,
-        "average_GPA": 3.5,
-    },{
-        "name" : "Houston Christian University",
-        "location": "Houston, TX",
-        "SAT_range": 1060,
-        "average_GPA": 3.41,
-    },{
-        "name" : "Rice University",
-        "location": "Houston, TX",
-        "SAT_range": 1520,
-        "average_GPA": 3.96,
-    }
-
+    "name": "University of Houston",
+    "location": "Houston, TX",
+    "SAT_range": 1240,
+    "average_GPA": 3.5,
+}, {
+    "name": "Houston Community College",
+    "location": "Houston, TX",
+    "SAT_range": 1190,
+    "average_GPA": 3.5,
+}, {
+    "name": "University of Houston–Downtown",
+    "location": "Houston, TX",
+    "SAT_range": 1240,
+    "average_GPA": 2.1,
+}, {
+    "name": "Baylor College of Medicine",
+    "location": "Houston, TX",
+    "SAT_range": 1430,
+    "average_GPA": 3.91,
+}, {
+    "name": "University of Houston–The Hill",
+    "location": "Houston, TX",
+    "SAT_range": 1240,
+    "average_GPA": 3.5,
+}, {
+    "name": "University of Houston–Clear Lake",
+    "location": "Houston, TX",
+    "SAT_range": 1240,
+    "average_GPA": 3.5,
+}, {
+    "name": "University of Texas Health Science Center at Houston",
+    "location": "Houston, TX",
+    "SAT_range": 1190,
+    "average_GPA": 3.5,
+}, {
+    "name": "Houston Christian University",
+    "location": "Houston, TX",
+    "SAT_range": 1060,
+    "average_GPA": 3.41,
+}, {
+    "name": "Rice University",
+    "location": "Houston, TX",
+    "SAT_range": 1520,
+    "average_GPA": 3.96,
+}
 
 dallas_fort = {
     'name': "Dallas Baptist University",
     'location': "Dallas, TX",
-    'average_GPA': 3.5, 
-    'SAT_range': 1100   
-},{
+    'average_GPA': 3.5,
+    'SAT_range': 1100
+}, {
     'name': "Southern Methodist University",
     'location': "Dallas, TX",
-    'average_GPA': 3.7,  
-    'SAT_range': 1300 
-},{
+    'average_GPA': 3.7,
+    'SAT_range': 1300
+}, {
     'name': "Tarrant County College",
     'location': "Fort Worth, TX",
-    'average_GPA': 3.0, 
-    'SAT_range': 900   
-},{
+    'average_GPA': 3.0,
+    'SAT_range': 900
+}, {
     'name': "Texas Christian University",
     'location': "Fort Worth, TX",
-    'average_GPA': 3.6, 
-    'SAT_range': 1150  
-},{
+    'average_GPA': 3.6,
+    'SAT_range': 1150
+}, {
     'name': "University of Texas at Dallas",
     'location': "Richardson, TX",
-    'average_GPA': 3.8,  
-    'SAT_range': 1350   
+    'average_GPA': 3.8,
+    'SAT_range': 1350
 }
 
-
 destin = {
-    "name" : "University of North Texas",
+    "name": "University of North Texas",
     "location": "Destin, TX",
     "SAT_range": 1355,
     "average_GPA": 3.46,
-},{
+}, {
     "name": "Texas Woman’s University",
     "location": "Destin, TX",
     "SAT_range": 1080,
@@ -107,81 +106,80 @@ san_antonio = {
     "location": "San Antonio, TX",
     "SAT_range": 1230,
     "average_GPA": 3.73,
-},{
+}, {
     "name": "University of the Incarnate Word",
     "location": "San Antonio, TX",
-    "SAT_range": 1020,  
-    "average_GPA": 3.5, 
-},{
+    "SAT_range": 1020,
+    "average_GPA": 3.5,
+}, {
     "name": "Alamo Colleges District",
     "location": "San Antonio, TX",
-    "SAT_range": 1070,  
+    "SAT_range": 1070,
     "average_GPA": 2.0,
-},{
+}, {
     "name": "San Antonio College",
     "location": "San Antonio, TX",
-    "SAT_range": 1180,  
+    "SAT_range": 1180,
     "average_GPA": 2.9,
 }
 
-corpus_christi =   {
+corpus_christi = {
     "name": "Texas A&M University–Corpus Christi",
     "location": "Corpus Christi, TX",
-    "SAT_range": 1080,  
-    "average_GPA": 3.26,  
-},{
+    "SAT_range": 1080,
+    "average_GPA": 3.26,
+}, {
     "name": "Del Mar College",
     "location": "Corpus Christi, TX",
     "SAT_range": 1010,
-    "average_GPA": 2.0,  
+    "average_GPA": 2.0,
 }
 
 waco_kileen = {
     "name": "Central Texas College",
     "location": "Killeen, TX",
-    "SAT_range": 1255, 
-    "average_GPA": 2.8,  
-},{
+    "SAT_range": 1255,
+    "average_GPA": 2.8,
+}, {
     "name": "McLennan Community College",
     "location": "Waco, TX",
-    "SAT_range": 1050, 
-    "average_GPA": 2.75, 
-},{
+    "SAT_range": 1050,
+    "average_GPA": 2.75,
+}, {
     "name": "Baylor University",
     "location": "Waco, TX",
-    "SAT_range": 1267, 
-    "average_GPA": 3.74, 
+    "SAT_range": 1267,
+    "average_GPA": 3.74,
 }
 
 others = {
     'name': "Texas Tech University",
     'location': "Lubbock, TX",
-    'average_GPA': 3.5,  
-    'SAT_range': 1150  
-},{
+    'average_GPA': 3.5,
+    'SAT_range': 1150
+}, {
     'name': "University of Texas at Arlington",
     'location': "Arlington, TX",
-    'average_GPA': 3.4,  
-    'SAT_range': 1100  
-},{
+    'average_GPA': 3.4,
+    'SAT_range': 1100
+}, {
     'name': "Texas State University",
     'location': "San Marcos, TX",
-    'average_GPA': 3.3,  
-    'SAT_range': 1050  
-},{
+    'average_GPA': 3.3,
+    'SAT_range': 1050
+}, {
     'name': "University of Texas at El Paso",
     'location': "El Paso, TX",
-    'average_GPA': 3.2, 
-    'SAT_range': 1000  
-},{
+    'average_GPA': 3.2,
+    'SAT_range': 1000
+}, {
     'name': "Angelo State University",
     'location': "San Angelo, TX",
-    'average_GPA': 3.1, 
-    'SAT_range': 950  
-},{
+    'average_GPA': 3.1,
+    'SAT_range': 950
+}, {
     'name': "Abilene Christian University",
     'location': "Abilene, TX",
-    'average_GPA': 3.6,  
-    'SAT_range': 1160  
+    'average_GPA': 3.6,
+    'SAT_range': 1160
 }
-
