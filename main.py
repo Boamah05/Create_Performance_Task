46b235cd6e2d63e2eae29e302256b148fe094c60
Boamah05 <143358964+Boamah05@users.noreply.github.com>
2024-04-09 12:44:28 +0000

    UI improvement in main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ game = True
 while game == True:
   f.main()
   gpa, sat, area = f.ask()
+  print('___________________________________________')
   gpa, sat, area = f.verify(gpa, sat, area)
+  print('___________________________________________')
   f.fetch_college(area, gpa, sat)
   game = False
